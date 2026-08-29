53f341b33308acc5afc0b39863a1995599e2ce31
rerarkkkkch <165208496+rerarkkkkch@users.noreply.github.com>
2026-08-29 14:28:57 +0000

    just a draft 1 without anything spesiallll

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+#default page config
+st.set_page_config(
+    layout="wide",
+    page_title="Evogen primer search",
+    page_icon="♿"
 )
+#main browser look
+st.title("Evogen primer search")
+st.write("Write down coordinates as in example: ChrX:12345-12346")
+st.markdown("zip zap")
+col1, col2, col3 = st.columns([1.5, 4, 1])
+with col1:
+    category = st.selectbox(
+        "Grch38/Grch37",
+        options=["Grch38", "Grch37"],
+        label_visibility="collapsed",
+        key="category_main"
+    )
+with col2:
+    coordinates = st.text_input(
+        "Coordinates",
+        placeholder="ChrX:12345-12346",
+        label_visibility="collapsed",
+        key="coordinates_main"
+    )
+with col3:
+    if st.button("🔍", use_container_width=True, key="search_btn"):
+        st.session_state.search_triggered = True
